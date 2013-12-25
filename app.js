2655b5578bd77ea1bd313e260a3efbf50fb71788
Andrew Montgomery-Hurrell <darkliquid@darkliquid.co.uk>
2013-12-25 11:31:06 +0000

    Style tweaks and power gen

--- app.js
+++ app.js
@@ -33,7 +33,21 @@ function loadCGHubImagePromise() {
 }
 
 function generatePowers() {
-    $('#creature').append('<p class="power">Power goes here</p>');
+    var powers = ["altering it's appearance", "becoming gigantic", "becoming tiny", "breathing underwater", "camoflaging it's body", "controlling air", "controlling arachnids", "controlling earth", "controlling electricity", "controlling fire", "controlling insects", "controlling water", "creating fire", "creating force fields", "detecting lies", "draining life energy", "eating anything", "an elastic body", "an enhanced sense of smell", "flight", "freezing things", "healing quickly", "healing others", "high jumping", "imitating voices", "immunity to most diseases", "immunity to most forms of radiation", "immunity to most toxins", "invisibility", "near-invulnerability to physical damage", "learning the complete history of an object by touching it", "manipulating emotions", "manipulating gravity", "manipulating light", "manipulating metal", "manipulating plants", "manipulating shadows", "manipulating time", "passing through solid objects", "prehensile hair", "a prehensile tail", "a prehensile tongue", "projecting illusions", "rapid learning", "reading minds", "seeing in the dark", "seeing the future", "seeing through illusions", "seeing through solid objects", "seeing remote events", "sensing emotions", "shooting energy beams", "super aiming skills", "super balance", "super hearing", "super intelligence", "super memory", "super navigation skills", "super speed", "super strength", "telepathy", "teleportation", "transforming into a gaseous form", "transforming into a liquid form", "withstanding extreme cold", "withstanding extreme heat"],
+    num_powers = getRandomInt(1, 3),
+    power_string = '',
+    selected_powers = [];
+    
+    for(var i = 0; i <= num_powers; i++) {
+        var index = Math.floor(Math.random()*powers.length);
+        if(i == num_powers) {
+            selected_powers = [selected_powers.join(', ')];
+            selected_powers.push('and');
+        }
+        selected_powers.push(powers.splice(index, 1)[0]);
+    }
+
+    $('#creature').append('<p class="power">This creature has the power of '+selected_powers.join(' ')+'</p>');
 }
 
 function generateStats() {
